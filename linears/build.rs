106f10af63707b0c9e21db735b49535f2b4748cb
Yoni Peleg <yoni.peleg@nanit.com>
2025-11-30 22:26:09 +0200

    Update build dependencies

--- linears/build.rs
+++ linears/build.rs
@@ -1,9 +1,7 @@
-
 const LIBLINEAR_REPO: &str = "https://github.com/cjlin1/liblinear.git";
 const LIBLINEAR_TAG: &str = "v245";
 
 fn main() {
-
     let tempdir = tempfile::tempdir().unwrap();
 
     let repo = git2::Repository::clone(LIBLINEAR_REPO, tempdir.path()).unwrap();
@@ -15,21 +13,26 @@ fn main() {
         Some(gref) => repo.set_head(gref.name().unwrap()),
         // this is a commit, not a reference
         None => repo.set_head_detached(obj.id()),
-    }.unwrap();
+    }
+    .unwrap();
 
     let p = tempdir.path();
 
     let mut b = cc::Build::new();
-        b.cpp(true)
-        .flag("-O2");
+    b.cpp(true).flag("-O2");
 
-    for f in &["linear.cpp", "newton.cpp", "blas/daxpy.c", "blas/ddot.c", "blas/dnrm2.c", "blas/dscal.c"] {
+    for f in &[
+        "linear.cpp",
+        "newton.cpp",
+        "blas/daxpy.c",
+        "blas/ddot.c",
+        "blas/dnrm2.c",
+        "blas/dscal.c",
+    ] {
         b.file(p.join(f));
     }
-        
-        b.include(&p)
-        .include(p.join("blas"))
-        .compile("liblinear");
+
+    b.include(&p).include(p.join("blas")).compile("liblinear");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
@@ -43,7 +46,7 @@ fn main() {
         .header(p.join("linear.h").to_str().unwrap())
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
